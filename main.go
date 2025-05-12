db2e6d16f72345be930712608f10132221ac0d3b
Mazhar <mazharulnihadmd@gmail.com>
2025-05-12 18:50:06 +0600

    cors added

--- main.go
+++ main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
 
-	"github.com/gorilla/mux"
-	"mongo-rest-api/db"
-	"mongo-rest-api/handlers"
+  "github.com/gorilla/mux"
+  "github.com/rs/cors"
+  "mongo-rest-api/db"
+  "mongo-rest-api/handlers"
 )
 
 func main() {
-	// connect to Mongo and populate db.Collection
-	db.Connect()
+  // 1. connect to MongoDB
+  db.Connect()
 
-	r := mux.NewRouter()
-	r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
-	r.HandleFunc("/users", handlers.GetUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
-	r.HandleFunc("/users/{id}", handlers.UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", handlers.DeleteUser).Methods("DELETE")
+  // 2. set up router
+  r := mux.NewRouter()
+  r.HandleFunc("/users", handlers.CreateUser).Methods("POST")
+  r.HandleFunc("/users", handlers.GetUsers).Methods("GET")
+  r.HandleFunc("/users/{id}", handlers.GetUser).Methods("GET")
+  r.HandleFunc("/users/{id}", handlers.UpdateUser).Methods("PUT")
+  r.HandleFunc("/users/{id}", handlers.DeleteUser).Methods("DELETE")
 
-	addr := ":8080"
-	fmt.Println("Server running at", addr)
-	log.Fatal(http.ListenAndServe(addr, r))
+  // 3. configure CORS
+  c := cors.New(cors.Options{
+    AllowedOrigins:   []string{"*"},          // allow all, or list your domains
+    AllowedMethods:   []string{"GET","POST","PUT","DELETE","OPTIONS"},
+    AllowedHeaders:   []string{"Authorization","Content-Type"},
+    AllowCredentials: true,
+  })
+
+  // 4. wrap router with CORS middleware
+  handler := c.Handler(r)
+
+  addr := ":8080"
+  fmt.Println("Server running at", addr)
+  log.Fatal(http.ListenAndServe(addr, handler))
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+mongo-rest-api/
+├── main.go
+├── db/
+│   └── mongo.go
+├── models/
+│   └── user.go
+└── handlers/
+|    └── user.go
+|___.env
+|___.gitignore
+|___readme.md
